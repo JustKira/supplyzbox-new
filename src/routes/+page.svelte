0944aa8460bfd9b809fa42fefe6a53619beb3d74
JustKira <shighgamerhg@gmail.com>
2025-02-09 20:38:38 +0200

    feat: initialize shadcn & added initial landing page ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import * as m from '$lib/paraglide/messages';
+	import * as Select from '$lib/components/ui/select';
+	import { Label } from '$lib/components/ui/label';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="flex h-[calc(100vh-theme(space.12))] items-center justify-center p-2 md:py-24">
+	<section
+		class="container flex h-full flex-1 flex-col justify-between gap-2 rounded-lg border bg-muted/25 p-2"
+	>
+		<div class="flex flex-col gap-2 rounded-md border bg-background p-4">
+			<h1 class="text-xl font-black uppercase md:text-4xl">
+				{m.hero_title()}
+			</h1>
+			<h2 class="text-xs text-muted-foreground md:text-sm">
+				{m.hero_subtitle()}
+			</h2>
+		</div>
+
+		<div class="flex w-full flex-col gap-2 rounded-md border bg-background p-4">
+			<h2 class="text-lg font-bold uppercase md:text-xl">{m.order_now()}</h2>
+
+			<div class="flex w-full flex-col items-end gap-4 sm:flex-row">
+				<div class="w-full min-w-40 sm:max-w-80">
+					<Label>
+						{m.school()}
+					</Label>
+					<Select.Root>
+						<Select.Trigger class="h-12 rounded-full bg-muted/50 sm:h-14">
+							{m.select_school()}
+						</Select.Trigger>
+						<Select.Content class="Content"></Select.Content>
+					</Select.Root>
+				</div>
+				<div class="w-full sm:max-w-80">
+					<Label>
+						{m.grade()}
+					</Label>
+					<Select.Root>
+						<Select.Trigger class="h-12 rounded-full bg-muted/50 sm:h-14">
+							{m.select_grade()}
+						</Select.Trigger>
+						<Select.Content class="Content"></Select.Content>
+					</Select.Root>
+				</div>
+				<Button class="h-12 w-full rounded-full text-lg sm:h-14 sm:max-w-52">{m.next()}</Button>
+			</div>
+		</div>
+	</section>
+</main>
---
+++ src/routes/navbar/navbar.svelte
@@ -0,0 +1,5 @@
+<div class="sticky top-0 h-12 w-full border-b">
+	<nav class="flex h-full items-center justify-between px-6 md:px-10">
+		<img src="logo.svg" alt="logo" class="size-10" />
+	</nav>
+</div>
